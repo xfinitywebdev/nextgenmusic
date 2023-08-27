def6c8906d6b198e5841ebd9e9b10ecca33092c1
xfinitywebdev <114882868+xfinitywebdev@users.noreply.github.com>
2023-08-27 02:12:21 +0000

    Committed change.

---
+++ portal/Registration/processor.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Account created!</title>
+  </head>
+  <body>
+    <h1>Your account has been created!</h1>
+    <p>Your info is processed in our database. You will now have newly released features as of now. You may click the 'Go back' button to go back to the home page.</p>
+    <a href="/index.html"><button>Go back to home!</button></a>
+  </body>
+</html>
+
+<?php
+  
+  
+    
+?>
