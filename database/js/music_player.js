d756bba07e9c1c131e7ffef02715c52308b1fa99
Drake <114882868+xfinitywebdev@users.noreply.github.com>
2025-01-07 01:49:43 +0000

    Test...

--- database/js/music_player.js
+++ database/js/music_player.js
@@ -1,5 +1,14 @@
-var infoFile = fetch("database/artist/rtm/albums/the return/the hawk/info.json")
 
 function loadContents() {
-    console.log(infoFile)
+    var infoFile = fetch("database/artist/rtm/albums/the return/the hawk/info.json")
+    fetch('database/artist/rtm/albums/the return/the hawk/info.json')
+  .then(response => response.json())
+  .then(data => {
+    // Do something with the JSON data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error(error);
+  });
 }
